d01cd1e775930ee41c8d0cca32a5c81f8b825140
SeongHyun Park <okpc0306@naver.com>
2021-03-29 01:16:48 +0900

    first commit

---
+++ level1_twoPlus.js
@@ -0,0 +1,26 @@
+function solution(numbers) {
+  var answer = [];
+  var sum = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers.length == i + 1) {
+      break;
+    }
+    for (var j = 0; j < numbers.length; j++) {
+      if (i != j) {
+        sum = numbers[i] + numbers[j];
+        answer.push(sum);
+      }
+    }
+  }
+  answer = new Set(
+    answer.sort(function (a, b) {
+      if (a > b) return 1;
+      if (a === b) return 0;
+      if (a < b) return -1;
+    })
+  );
+  answer = [...answer];
+  return answer;
+}
+var numbers = 0;
+console.log(solution(numbers));
